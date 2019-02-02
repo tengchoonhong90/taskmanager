520710d35290e1a805ebba0795bb77ebcf70fb01
damienteo <ksteo89@gmail.com>
2019-02-03 00:49:09 +0800

    Customised Error Messages for New Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,9 @@
 class Task < ApplicationRecord
     has_and_belongs_to_many :users
-    validates :task_name, presence: true, length: { minimum: 3}
-    validates :location, presence: true, numericality: { only_integer: true }, length: { minimum: 6, maximum: 6}
-    validates :task_description, presence: true, length: { minimum: 10}
-    validates :price, presence: true, numericality: { only_integer: true }
-    validates :negotiable, {presence: true}
+    validates :task_name, presence: { message: "Please input a name for your task" }, length: { minimum: 8, message: "The given name is too short. At least 8 characters required."}
+    validates :location, presence: { message: "Please input a postal code for your task" }, numericality: { only_integer: true, message: "The postal code is made of numbers"}, length: { minimum: 6, maximum: 6, message: "Postal codes have only 6 digits."}
+    validates :task_description, presence: { message: "Please describe the task" }, length: { minimum: 12, message: "Please elaborate more on how the task is meant to be done."}
+    validates :price, presence: { message: "Please input a price. Lowest is $0" }, numericality: { only_float: true, message: "Please input a valid number."}
+    validates :negotiable, {presence: {message: "Please let us know whether you want to allow potential helpers to bid for prices."}}
     
 end
